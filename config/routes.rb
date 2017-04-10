88dbca671e922957334f29c13b797d04611a190e
LionGS <gb1541@likelion.org>
2017-04-10 20:47:18 +0900

    자바 스크립트 추가 완료, 리셋후 버튼 먹통

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => "drawing_lots#home"
+  get "/home" => "drawing_lots#home"
 end
